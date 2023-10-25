6cc39df87928ffd8af00c18bdf2a35391e5646a0
Carlos Quesada <carlosquesadaumana@gmail.com>
2023-10-25 11:21:38 -0600

    Account added

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,34 @@ function App() {
     <>
       <Sidebar/>
 
-      <main className='p-4 sm:ml-20 md:ml-20 xl:ml-56'>
+      <main className='p-4 sm:ml-20 md:ml-20 xl:ml-56 flex m-auto'>
         <PostsList />
+
+        <aside
+          className='hidden lg:flex ml-[64px] w-[319px] h-[518px]'
+        >
+          <div className="flex h-[44px] justify-between w-full">
+            <div className="flex justify-between">
+              <img 
+                className='w-8 h-8 rounded-full' 
+                src="../../../public/profile-image.jpg" 
+                alt="" 
+              />
+
+              
+              <div className='ml-2'>
+                <h2 className='text-[15px] font-bold'>Username</h2>
+                <p className='text-[15px]'>Carlos H Quesada</p>
+              </div>
+            </div>
+            <span className='text-[15px] mt-auto'>Switch</span>
+          </div>
+
+          
+        </aside>
       </main>
+
+      
     </>
   )
 }
