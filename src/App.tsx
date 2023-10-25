7207a3995deccd474bcdcb0921600a85a4000fa1
Carlos Quesada <carlosquesadaumana@gmail.com>
2023-10-24 18:32:25 -0600

    Post list and card template done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Sidebar from 'components/Sidebar/Sidebar'
+import PostsList from 'modules/Posts/PostsList/PostsList'
+
 
 function App() {
   return (
@@ -9,6 +11,10 @@ function App() {
       <main className='p-4 sm:ml-20 md:ml-20 xl:ml-56'>
         <h1>Instagram clone</h1>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde, saepe perferendis molestiae placeat asperiores, iusto nisi mollitia quas eligendi tenetur quo amet. Eveniet quasi totam adipisci deserunt nam neque dolores!</p>
+      
+        <hr />
+
+        <PostsList />
       </main>
     </>
   )
