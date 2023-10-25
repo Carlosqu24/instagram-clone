ed23529b2ad5f93e46de71b87f87afcc995cbbf2
Carlos Quesada <carlosquesadaumana@gmail.com>
2023-10-25 10:45:12 -0600

    Unused text removed from App component

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,6 @@ function App() {
       <Sidebar/>
 
       <main className='p-4 sm:ml-20 md:ml-20 xl:ml-56'>
-        <h1>Instagram clone</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde, saepe perferendis molestiae placeat asperiores, iusto nisi mollitia quas eligendi tenetur quo amet. Eveniet quasi totam adipisci deserunt nam neque dolores!</p>
-      
-        <hr />
-
         <PostsList />
       </main>
     </>
