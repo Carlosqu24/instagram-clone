b448f5d754f716a862da9e49d82f13f7ae8d7221
Carlos Quesada <carlosquesadaumana@gmail.com>
2023-10-26 08:36:59 -0600

    Test added

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,27 @@ import React from 'react'
 import Sidebar from 'components/Sidebar/Sidebar'
 import PostsList from 'modules/Posts/PostsList/PostsList'
 import RightSidebar from 'components/RightSidebar/RightSidebar'
-
+import UserProfile from 'modules/User/UserProfile/UserProfile'
 
 function App() {
   return (
     <>
       <Sidebar />
 
-      <main className='p-4 sm:ml-20 md:ml-20 xl:ml-56 flex m-auto'>
-        <PostsList />
+      <main className="p-4 sm:ml-20 md:ml-20 xl:ml-56 flex m-auto">
+        {/* <PostsList /> */}
+
+        <UserProfile
+          user={{
+            id: crypto.randomUUID(),
+            userName: 'carlos_qu24',
+            displayName: 'Carlos H. Quesada',
+            biography: 'Web developer | Web designer P.Z. 🌊🌴'
+          }}
+        />
 
         <RightSidebar />
       </main>
-
-
     </>
   )
 }
