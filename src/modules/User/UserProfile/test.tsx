a84feb366ea5cb0306d32fac82d266d9317079d9
Carlos Quesada <carlosquesadaumana@gmail.com>
2023-10-26 02:08:13 -0600

    UserProfile test file added

---
+++ src/modules/User/UserProfile/test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react'
+import UserProfile from './UserProfile'
+
+const user = {
+  id: crypto.randomUUID(),
+  userName: 'carlos_qu24',
+  displayName: 'Carlos H. Quesada',
+  biography: 'Web developer | Web designer P.Z. 🌊🌴'
+}
+
+describe('UserProfile', () => {
+  it('Render Profile', () => {
+    const { container } = render(<UserProfile user={user} />)
+
+    expect(screen.getByText(user.userName)).toBeInTheDocument()
+    expect(screen.getByText(user.biography)).toBeInTheDocument()
+  })
+})
