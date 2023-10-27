a99ce0dad4429012c29682c81404fe810a3f5d25
Carlos Quesada <carlosquesadaumana@gmail.com>
2023-10-27 04:12:15 -0600

    RouterProvider added in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { RouterProvider } from 'react-router-dom'
 import Sidebar from 'components/Sidebar/Sidebar'
 import PostsList from 'modules/Posts/PostsList/PostsList'
 import RightSidebar from 'components/RightSidebar/RightSidebar'
 import UserProfile from 'modules/User/UserProfile/UserProfile'
 import 'flowbite'
+import { router } from 'routes'
 
 function App() {
   return (
@@ -13,14 +15,7 @@ function App() {
       <main className="p-4 sm:ml-20 md:ml-20 xl:ml-56 flex m-auto">
         {/* <PostsList /> */}
 
-        <UserProfile
-          user={{
-            id: crypto.randomUUID(),
-            userName: 'carlos_qu24',
-            displayName: 'Carlos H. Quesada',
-            biography: 'Web developer | Web designer P.Z. 🌊🌴'
-          }}
-        />
+        <RouterProvider router={router} />
 
         {/* <RightSidebar /> */}
       </main>
