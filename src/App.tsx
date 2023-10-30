0178db76f9dff01ef0467a5775d96095ad682420
Carlos Quesada <carlosquesadaumana@gmail.com>
2023-10-30 10:53:33 -0600

    Main padding 0 added in sm screen size

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <>
       <Sidebar />
 
-      <main className="p-4 sm:ml-20 md:ml-20 xl:ml-56 flex m-auto">
+      <main className="p-0 sm:p-4 md:p-4 lg:p-4 xl:p-4 2xl:p-4 sm:ml-20 md:ml-20 xl:ml-56 flex m-auto">
         {/* <PostsList /> */}
 
         <RouterProvider router={router} />
