4e456ff8ebff506a340de5a6c32ac30d4975d61b
Carlos Quesada <carlosquesadaumana@gmail.com>
2023-10-31 17:05:21 -0600

    UserInformationHeader styles fixed

--- src/modules/User/components/UserInformation/UserInformation.tsx
+++ src/modules/User/components/UserInformation/UserInformation.tsx
@@ -11,32 +11,42 @@ interface UserProfileProps {
   user: User
 }
 
+const userInformationClassNames = {
+  userInformationContainer: '',
+  profileImage:
+    'md:ml-[47.656px] md:mr-[77.656px] mr-[28px] rounded-full object-cover 77 w-[77px] h-[77px] md:w-[150px] md:h-[150px]'
+}
+
 const UserInformation = ({ user }: UserProfileProps) => {
   return (
-    <div className="flex mb-[44px]">
+    <div className="flex md:mb-[44px] m-[16px]">
       <img
         src="../../../public/profile-image.jpg"
         alt=""
-        className=" ml-[47.656px] mr-[77.656px] rounded-full object-cover 77 w-[150px] h-[150px]"
+        className={userInformationClassNames.profileImage}
       />
 
       <div>
-        <div className="mb-[20px] flex items-center">
-          <span className="mr-[20px] text-[18px]">{user.userName}</span>
+        <div className="mb-[20px] flex flex-wrap items-center">
+          <span className="mr-[20px] text-[18px] w-[100%] md:w-[auto] mb-[12px] md:mb-[0]">
+            {user.userName}
+          </span>
           <button className="mr-[8px] px-[16px] py-[3px] bg-[#262626] rounded-[8px] text-[14px]">
             Edit profile
           </button>
           <button className="mr-[12px] px-[16px] py-[3px] bg-[#262626] rounded-[8px] text-[14px]">
             View archive
           </button>
-          <span className="material-icons">
-          settings
-          </span>
+          <span className="material-icons">settings</span>
         </div>
         <div className="mb-[20px]">
-          <span className="mr-[40px] text-[16px]">66 posts</span>
-          <span className="mr-[40px] text-[16px]">233 followers</span>
-          <span className='text-[16px]'>564 following</span>
+          <span className="mr-[40px] text-[16px] hidden md:inline">
+            66 posts
+          </span>
+          <span className="mr-[40px] text-[16px] hidden md:inline">
+            233 followers
+          </span>
+          <span className="text-[16px] hidden md:inline">564 following</span>
         </div>
         <p className="mb-[6px]">{user.displayName}</p>
         <p>{user.biography}</p>
