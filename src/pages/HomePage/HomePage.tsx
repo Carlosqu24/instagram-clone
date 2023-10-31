d7cb36bad7a774c8fc1b18de09d49188c2584008
Carlos Quesada <carlosquesadaumana@gmail.com>
2023-10-31 16:33:19 -0600

    PostList container and presentation names changes to Posts

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,12 @@
 import RightSidebar from 'components/RightSidebar/RightSidebar'
 import PostsList from 'modules/Posts/components/PostsList/PostsList'
-import PostListContainer from 'modules/Posts/containers/PostsListContainer/PostListContainer'
+import PostsContainer from 'modules/Posts/containers/PostsListContainer/PostListContainer'
 import React from 'react'
 
 const HomePage = () => {
   return (
     <>
-      <PostListContainer />
+      <PostsContainer />
       <RightSidebar />
     </>
   )
