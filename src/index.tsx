1526199008485a52e52f649be75cdcbd95aa2785
Carlos Quesada <carlosquesadaumana@gmail.com>
2023-11-08 04:53:27 -0600

    i18n provider added

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,14 @@ import 'tailwindcss/tailwind.css'
 import App from 'App'
 
 import './config/i18n/index'
+import { I18nextProvider } from 'react-i18next'
+import i18n from './config/i18n/index'
 
 const container = document.getElementById('root') as HTMLDivElement
 const root = createRoot(container)
 
-root.render(<App />)
+root.render(
+  <I18nextProvider i18n={i18n}>
+    <App />
+  </I18nextProvider>
+)
