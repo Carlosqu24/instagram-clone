dbe2b875ff7d56d1aecaea792e462076b7e6762a
Carlos Quesada <carlosquesadaumana@gmail.com>
2023-11-17 02:18:53 -0600

    CreatePostForm component and test created

---
+++ test/modules/Posts/CreatePost/test.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { describe, expect, test } from 'vitest'
+import customRender from '../../../utils'
+
+import { screen } from '@testing-library/react'
+
+describe('Create post', () => {
+  test('Should render all posts', () => {
+    // const { container } = customRender(<HomePage />)
+
+    // screen.getAllByText('lamborghini')
+    // screen.getAllByText('Awesome description')
+    // screen.getAllByText('0 likes')
+
+    expect(true).toBe(true)
+  })
+})
