f415d2937a9fe1d982da703cc7ca2b7f60d25c52
Carlos Quesada <carlosquesadaumana@gmail.com>
2023-11-21 05:14:40 -0600

    Modal classNames dinamically

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,19 +1,38 @@
 import React, { ReactNode } from 'react'
 
+export interface ModalStyles {
+  modalContainer: string
+  modalHeader: string
+  modalContent: string
+}
+
 interface ModalProps {
   isOpen: boolean
   onClose: Function
   children: ReactNode
-  title: string
+  header: ReactNode
+  modalStyles?: ModalStyles
 }
 
-const Modal = ({ isOpen, onClose, children, title }: ModalProps) => {
+const Modal = ({
+  isOpen,
+  onClose,
+  children,
+  header,
+  modalStyles
+}: ModalProps) => {
   if (!isOpen) return null
 
   return (
     <div className="fixed inset-0 flex items-center justify-center z-10000000">
       <div className="absolute bg-gray-800 bg-opacity-75 inset-0"></div>
-      <div className="z-10 bg-white max-w-md mx-auto rounded-md shadow-lg w-[440px] h-[440px] z-10000000">
+      <div
+        className={
+          modalStyles && modalStyles.modalContainer
+            ? modalStyles.modalContainer
+            : 'z-50 bg-white mx-auto rounded-md shadow-lg w-[440px] h-[440px] z-10000000'
+        }
+      >
         <button
           className="absolute top-4 right-4 text-gray-700 hover:text-gray-900"
           onClick={() => onClose()}
@@ -21,25 +40,27 @@ const Modal = ({ isOpen, onClose, children, title }: ModalProps) => {
           X
         </button>
 
-        <div className="p-2 text-center">{title}</div>
+        <div
+          className={
+            modalStyles && modalStyles.modalHeader
+              ? modalStyles.modalHeader
+              : 'p-2 w-full text-center flex justify-content-between'
+          }
+        >
+          {header}
+        </div>
 
-        <div className="modal-content p-8">{children}</div>
+        <div
+          className={
+            modalStyles && modalStyles.modalContent
+              ? modalStyles.modalContent
+              : 'modal-content flex justify-content-between'
+          }
+        >
+          {children}
+        </div>
       </div>
     </div>
-
-    // <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-    //   <div className="bg-white w-full max-w-md p-4 rounded-md">
-    //     <div className="flex justify-end">
-    //       <button
-    //         className="text-gray-500 hover:text-gray-700"
-    //         onClick={() => onClose()}
-    //       >
-    //         &times;
-    //       </button>
-    //     </div>
-    //     <div className="mt-4">{children}</div>
-    //   </div>
-    // </div>
   )
 }
 
