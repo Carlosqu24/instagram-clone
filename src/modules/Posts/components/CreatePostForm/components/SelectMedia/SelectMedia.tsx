ad7db5b31f959be132ada9b94c43722fb90e44f7
Carlos Quesada <carlosquesadaumana@gmail.com>
2023-11-21 22:12:53 -0600

    SelectMedia component fixed

--- src/modules/Posts/components/CreatePostForm/components/SelectMedia/SelectMedia.tsx
+++ src/modules/Posts/components/CreatePostForm/components/SelectMedia/SelectMedia.tsx
@@ -1,9 +1,10 @@
+import React, { useState, useEffect, useContext } from 'react'
 import { useModal } from 'context/Modal/ModalContext'
-import React from 'react'
 import CreateNewPost, {
   CreateNewPostHeader
 } from '../CreateNewPost/CreateNewPost'
 import { ModalStyles } from 'components/Modal/Modal'
+import { FileContext } from 'context/FileReader/FileReaderContext'
 
 const modalStyles: ModalStyles = {
   modalContainer:
@@ -13,30 +14,35 @@ const modalStyles: ModalStyles = {
 }
 
 const SelectMedia = () => {
+  const { wasImageUploaded, handleFileChange } = useContext(FileContext)
+
+  const { openModal } = useModal()
+
+  useEffect(() => {
+    if (wasImageUploaded) {
+      openModal(<CreateNewPost />, <CreateNewPostHeader />, modalStyles)
+    }
+  }, [wasImageUploaded])
+
   return (
     <div className="flex flex-col h-full items-center justify-content-center">
       <h1 className="text-[20px]">Drag photos and videos here</h1>
-      <button className="bg-[#0095F6] text-[#fff] py-[7px] px-[16px] mt-[24px]">
-        Select From Computer
-      </button>
+
+      <input
+        type="file"
+        placeholder="Select From Computer"
+        onChange={handleFileChange}
+        id="fileInput"
+        className="bg-[#0095F6] text-[#fff] py-[7px] px-[16px] mt-[24px]"
+      />
     </div>
   )
 }
 
 export const SelectMediaHeader = () => {
-  const { openModal } = useModal()
-
   return (
     <div className="w-full flex items-center justify-content-between">
       <h1 className="text-center">Create new post</h1>
-      <button
-        className="ml-auto"
-        onClick={() =>
-          openModal(<CreateNewPost />, <CreateNewPostHeader />, modalStyles)
-        }
-      >
-        next
-      </button>
     </div>
   )
 }
