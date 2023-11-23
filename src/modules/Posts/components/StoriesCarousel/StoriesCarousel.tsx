facb7384b9a8a2419d4f762b07a0a5a051e72f47
Carlos Quesada <carlosquesadaumana@gmail.com>
2023-11-23 04:16:34 -0600

    StoriesCarousel fixed

--- src/modules/Posts/components/StoriesCarousel/StoriesCarousel.tsx
+++ src/modules/Posts/components/StoriesCarousel/StoriesCarousel.tsx
@@ -6,14 +6,14 @@ import blueOriginProfileImage from '../../../../../public/storiesList/blue_origi
 import appleProfileImage from '../../../../../public/storiesList/apple_profile_image.jpg'
 import maseratiProfileImage from '../../../../../public/storiesList/maserati_profile_image.webp'
 import githubProfileImage from '../../../../../public/storiesList/github_profile_image.png'
-import cr7ProfileImage from '../../../../../public/storiesList/cr7_profile_image.jpg'
+import tattoodoProfileImage from '../../../../../public/storiesList/tattoodo_profile_image.png'
 import messiProfileImage from '../../../../../public/storiesList/messi_profile_image.webp'
 
 const storiesList = [
   {
     id: crypto.randomUUID(),
     profileImageUrl: wildProjectProfileImage,
-    title: 'the.wildproject'
+    title: 'the.wildp...'
   },
   {
     id: crypto.randomUUID(),
@@ -23,7 +23,7 @@ const storiesList = [
   {
     id: crypto.randomUUID(),
     profileImageUrl: blueOriginProfileImage,
-    title: 'blueorigin'
+    title: 'blueorig...'
   },
   {
     id: crypto.randomUUID(),
@@ -47,14 +47,14 @@ const storiesList = [
   },
   {
     id: crypto.randomUUID(),
-    profileImageUrl: cr7ProfileImage,
-    title: 'cr7cristianoronaldo'
+    profileImageUrl: tattoodoProfileImage,
+    title: 'tattoodo'
   }
 ]
 
 const StoriesCarousel = () => {
   return (
-    <div className=" mx-auto mb-[48px] border-solid border-[1px] border-[#fff]">
+    <div className=" mx-auto mb-[48px] ">
       <ul className="  w-[100%] flex space-x-2 font-serif overflow-hidden">
         {storiesList.map((storie) => (
           <li className="flex flex-col items-center space-y-1 ">
@@ -64,7 +64,7 @@ const StoriesCarousel = () => {
                 href="/"
               >
                 <img
-                  className="max-w-none h-[56px] w-[56px] rounded-full object-cover"
+                  className="max-w-none h-[54.5px] w-[54.5px] rounded-full object-cover"
                   src={storie.profileImageUrl}
                   alt="cute kitty"
                 />
